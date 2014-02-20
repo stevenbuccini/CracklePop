f301ecb9f7e164c3df4dab4ae471df0d906b6036
stevenbuccini <steven@stevenbuccini.com>
2014-02-19 16:23:36 -0800

    Update fizzbuzz.py
    Updated FizzBuzz to CracklePop for the Hacker School Fa14 application.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,10 +6,10 @@
 def fizz_buzz():
 	for num in range(1, 101):
 		if num %  15 == 0:
-			print("FizzBuzz") #catch this case first, since things divisible by 3 and 5 won't make it to the 15 case if it's at the bottom
+			print("CracklePop") #catch this case first, since things divisible by 3 and 5 won't make it to the 15 case if it's at the bottom
 		elif num % 3 == 0:
-			print("Fizz")
+			print("Crackle")
 		elif num % 5 == 0:
-			print("Buzz")
+			print("Pop")
 		else:
 			print(str(num))
