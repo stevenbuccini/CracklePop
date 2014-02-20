ab255f6b90e58a2a788ea7f2b309c8a3a52ceab4
stevenbuccini <steven@stevenbuccini.com>
2014-02-19 16:25:06 -0800

    Update and rename fizzbuzz.py to cracklepop.py

---
+++ cracklepop.py
@@ -0,0 +1,17 @@
+##########################################################################
+## CracklePop                                                           ##
+##                                                                      ##
+## Coded with love by Steven Buccini for my Hacker School applicaiton   ##
+##########################################################################
+
+
+def fizz_buzz():
+	for num in range(1, 101):
+		if num %  15 == 0:
+			print("CracklePop") #catch this case first, since things divisible by 3 and 5 won't make it to the 15 case if it's at the bottom
+		elif num % 3 == 0:
+			print("Crackle")
+		elif num % 5 == 0:
+			print("Pop")
+		else:
+			print(str(num))
