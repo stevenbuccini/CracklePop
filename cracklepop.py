de059c94edf33e52c7750567c416c91cea74c695
stevenbuccini <steven@stevenbuccini.com>
2014-03-01 15:15:21 -0800

    Update cracklepop.py

--- cracklepop.py
+++ cracklepop.py
@@ -5,7 +5,7 @@
 ##########################################################################
 
 
-def fizz_buzz():
+def crackle_pop():
 	for num in range(1, 101):
 		if num %  15 == 0:
 			print("CracklePop") #catch this case first, since things divisible by 3 and 5 won't make it to the 15 case if it's at the bottom
